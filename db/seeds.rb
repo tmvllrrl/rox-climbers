10848972f834e3f2e557f0776b4ac7408bd51339
Michael Villarreal <tmvllrrl@memphis.edu>
2022-04-06 23:58:26 -0500

    Added Seeds to Databased

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+seed1 = Photo.create!(
+    route_title: "Hanging",
+    route_description: "Pretty fun, but pretty hard!",
+    route_grade: "V3",
+    route_location: "Middle Front",
+    route_style: "Cylinder Slopers"
+)
+seed1.route_img.attach(io: File.open('public/seed1.jpg'), filename: 'seed1.jpg', content_type: 'image/jpg')
+
+seed2 = Photo.create!(
+    route_title: "Hanging 2: Electric Boogaloo",
+    route_description: "Pretty fun, but pretty hard!",
+    route_grade: "V3",
+    route_location: "Middle Front",
+    route_style: "Cylinder Slopers"
+)
+seed2.route_img.attach(io: File.open('public/seed2.jpg'), filename: 'seed2.jpg', content_type: 'image/jpg')
+
+seed3 = Photo.create!(
+    route_title: "Slap that Rock",
+    route_description: "Not too bad, but as hard start",
+    route_grade: "V1",
+    route_location: "Sponge Boulder",
+    route_style: "Slopers/Jugs"
+)
+seed3.route_img.attach(io: File.open('public/seed3.jpg'), filename: 'seed3.jpg', content_type: 'image/jpg')
+
+seed4 = Photo.create!(
+    route_title: "Arete the Way",
+    route_description: "Pretty fun, but pretty hard!",
+    route_grade: "V2",
+    route_location: "Back Wall",
+    route_style: "Arete/Crimpy"
+)
+seed4.route_img.attach(io: File.open('public/seed4.jpg'), filename: 'seed4.jpg', content_type: 'image/jpg')
