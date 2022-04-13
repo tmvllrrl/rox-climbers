57d74fb11fcfe363f09a9ff5390822392643363d
Michael Villarreal <tmvllrrl@memphis.edu>
2022-04-13 14:00:49 -0500

    Style Changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,18 +2,27 @@ class PhotosController < ApplicationController
     before_action :authenticate_user!, except: [:index]
 
     def index
-        if user_signed_in? # If the user is signed in, then display their photos
+        if user_signed_in? # If the user is signed in, then display the photos of who they follow
             if params[:order] == "updated_at"
-                @photos = current_user.photos.order(created_at: :desc)
+                @follows = Follow.select(:follow_id).where(user_id: current_user.id)
+                @photos = Photo.where(user_id: @follows).order(created_at: :desc)
             elsif params[:order] == "route_grade"
-                @photos = current_user.photos.order(:route_grade)
+                @follows = Follow.select(:follow_id).where(user_id: current_user.id)
+                @photos = Photo.where(user_id: @follows).order(:route_grade)
+            elsif params[:order] == "route_grade_desc"
+                @follows = Follow.select(:follow_id).where(user_id: current_user.id)
+                @photos = Photo.where(user_id: @follows).order(route_grade: :desc)
             elsif params[:order] == "route_location"
-                @photos = current_user.photos.order(:route_location)
+                @follows = Follow.select(:follow_id).where(user_id: current_user.id)
+                @photos = Photo.where(user_id: @follows).order(:route_location)
             elsif params[:order] == "route_style"
-                @photos = current_user.photos.order(:route_style)
+                @follows = Follow.select(:follow_id).where(user_id: current_user.id)
+                @photos = Photo.where(user_id: @follows).order(:route_style)
             else # Default behavior
-                @photos = Photo.all
+                @follows = Follow.select(:follow_id).where(user_id: current_user.id)
+                @photos = Photo.where(user_id: @follows).order(created_at: :desc)
             end
+
             render :index
         else # If the user is not signed in, then display a "home page" user's photos
             @user = User.find(1)
@@ -27,19 +36,9 @@ class PhotosController < ApplicationController
             elsif params[:order] == "route_style"
                 @photos = @user.photos.order(:route_style)
             else # Default behavior
-                @photos = Photo.all
+                @photos = @user.photos.order(created_at: :desc)
             end
-            render :index
-        end
-    end
-
-    def index_sort_grade
-        if user_signed_in? # If the user is signed in, then display their photos
-            @photos = current_user.photos.order(:route_grade)
-            render :index
-        else # If the user is not signed in, then display a "home page" user's photos
-            @user = User.find(1)
-            @photos = @user.photos.order(:route_grade)
+            
             render :index
         end
     end
