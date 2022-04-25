ec153abfc9769baec84194137123769d6e23ed46
yuexinwu008 <yuexinwu008@gmail.com>
2022-04-24 20:30:46 -0500

    comment function

--- config/routes.rb
+++ config/routes.rb
@@ -21,13 +21,4 @@ Rails.application.routes.draw do
 
   get 'photos/:photo_id/likes' , to: 'likes#create'
 
-  
-  
-  
-  get 'explores/', to: 'explores#index', as: 'explores'
-
-  get 'location', to: 'maps#location', as: 'location'
-
- 
-
 end
