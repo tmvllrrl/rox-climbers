11227d715501adcfd31e07a709b5d29a514fd2ab
Michael Villarreal <tmvllrrl@memphis.edu>
2022-05-03 22:11:27 -0500

    Slight tweaks to links

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
     def create
         @favorite= Favorite.new(params.require(:favorite).permit(:user_id, :photo_id, :is_favorite))
         if @favorite.save
-            flash[:success] = "You have marked this phots as favorite"
+            flash[:success] = "You have favorited this photo."
             redirect_to photo_url(Photo.find(params[:photo_id]))
         else
         end
@@ -11,7 +11,7 @@ class FavoritesController < ApplicationController
     def destroy
         @favorite = Favorite.where(user_id: params[:user_id]).where(photo_id: params[:photo_id]).take
         @favorite.destroy
-        flash[:error] = "This photo is removed from favorite!"
+        flash[:error] = "This photo is removed from favorites!"
         redirect_to photo_url(Photo.find(params[:photo_id]))
     end
 
